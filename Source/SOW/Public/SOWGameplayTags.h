f7667fae246be0b201cc607d2cbe8a2689144413
wpdudH <66068922+wpdudH@users.noreply.github.com>
2025-07-05 22:29:51 +0900

    hjy/ fix: resolve merge conflicts

--- Source/SOW/Public/SOWGameplayTags.h
+++ Source/SOW/Public/SOWGameplayTags.h
@@ -19,6 +19,7 @@ namespace SOWGameplayTags {
 	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Ability_Summon);
 
 	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Status_Rolling);
+	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Status_Fail_Ability);
 
 	/* Enemy Tags */
 	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Ability_Attack_Melee);
@@ -96,6 +97,7 @@ namespace SOWGameplayTags {
 
 	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Shared_SetByCaller_ReductedDamage);
 
-
+	/* Data Tags */
+	SOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_ManaCost);
 	
 }
